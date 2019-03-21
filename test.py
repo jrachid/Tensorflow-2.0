36b6a321695ce841366c68c93a16840ea9f7ebd2
jrachid <rachidj@protonmail.ch>
2019-03-21 19:04:06 +0300

    first commit - Test wit fashion_MNIST

---
+++ test.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt 
+import pandas as pd 
+import numpy as np 
+import tensorflow as tf 
+import sys
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
