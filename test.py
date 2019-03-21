3b393558fa8068ec068f654093005087f73d20ef
jrachid <rachidj@protonmail.ch>
2019-03-21 20:22:56 +0300

    update

--- test.py
+++ test.py
@@ -11,6 +11,14 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 # Plot an image
-plt.imshow(images[10], cmap="binary")
-plt.title(class_names[targets[10]])
-plt.show()
+# plt.imshow(images[11], cmap="binary")
+# plt.title(class_names[targets[11]])
+# plt.show()
+
+# Create the model
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten(input_shape=[28,28]))
+
+print("Shape of the image", images[0:1].shape)
+model_output = model.predict(images[0:1])
+print("Shape of the image after the flatten", model_output.shape)
