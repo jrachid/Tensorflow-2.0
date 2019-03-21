30da018c8469036a4afcfba3616a3f0a2334d3f7
jrachid <rachidj@protonmail.ch>
2019-03-21 19:08:23 +0300

    update mnist data

--- test.py
+++ test.py
@@ -5,8 +5,12 @@ import tensorflow as tf
 import sys
 
 fashion_mnist = tf.keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+(images, targets), (_, _) = fashion_mnist.load_data()
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
+# Plot an image
+plt.imshow(images[10], cmap="binary")
+plt.title(class_names[targets[10]])
+plt.show()
