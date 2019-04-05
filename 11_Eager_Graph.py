25af5794f22894337362dea4440626e800f8980e
jrachid <rachidj@protonmail.ch>
2019-04-05 18:38:04 +0200

    update eager graph mode

--- 11_Eager_Graph.py
+++ 11_Eager_Graph.py
@@ -21,3 +21,26 @@ print(add_fc(a, b))
 # print(tf.autograph.to_code(add_fc))
 
 # Graph mode and eager mode with a Keras Model
+# Flatten
+model = tf.keras.models.Sequential()
+# Add layers
+model.add(tf.keras.layers.Dense(256, activation="relu"))
+model.add(tf.keras.layers.Dense(128, activation="relu"))
+model.add(tf.keras.layers.Dense(10, activation="softmax"))
+
+# 2 types of outputs
+# Array output
+model_output = model.predict(np.zeros((1,30)))
+print(model_output)
+
+# Tensor output
+model_output = model(np.zeros((1,30)))
+print(model_output)
+
+# We can execute the model in graph mode by using the @tf.function decorator
+@tf.function
+def predict(x):
+    return model(x)
+
+model_output = predict(np.zeros((1, 30))) 
+print(model_output)
